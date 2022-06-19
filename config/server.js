3c3f4583e2343ded6f07d9cdaf639a3151ce8b16
unknown <juantibaldo@gmail.com>
2022-06-18 21:49:41 -0300

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2e811d96c4c4ddeb384896c55404183d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f00c15cc-5423-41eb-aa63-e30ce13fdac2'
+};
